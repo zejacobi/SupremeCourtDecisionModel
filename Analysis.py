a1de48b48c9bec2969c5163f372115157679b820
zejacobi <zejacobi@gmail.com>
2014-11-26 17:19:22 -0500

    attempted to fix imported, added two prints

--- Analysis.py
+++ Analysis.py
@@ -2,4 +2,7 @@ __author__ = 'zach'
 
 from pandas import DataFrame as Df
 
-supreme_court_decisions = Df.from_csv('./Supreme Court.txt', sep=r'\t', header=1)
+supreme_court_decisions = Df.from_csv('./Supreme Court.txt', sep='\t')
+
+print len(supreme_court_decisions[supreme_court_decisions['Federal Government'] == 1])
+print len(supreme_court_decisions[supreme_court_decisions['Charter Right'] == 1])
